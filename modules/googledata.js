3cd82616640fa9ccb7e5f5b878a376456621bb76
humax <telediario@gmail.com>
2017-09-18 13:49:56 +0200

    Added support for additional images in architecture category

--- modules/googledata.js
+++ modules/googledata.js
@@ -1,11 +1,11 @@
 var GoogleSpreadsheet  = require("google-spreadsheet");
 var SiteData = {
-  imgs: []
+  cards: []
 };
 
 function getThumbnailUrl(url){
   // var thumbMediaProcessorPrefix = "https://mediaprocessor.websimages.com/width/328/crop/0,0,283x211/";
-  var thumbMediaProcessorPrefix = "https://mediaprocessor.websimages.com/width/300/";
+  var thumbMediaProcessorPrefix = "https://mediaprocessor.websimages.com/width/350/";
   var smImg;
 
   if ( url.toLowerCase().indexOf("http://") > -1 ) {
@@ -20,6 +20,77 @@ function getThumbnailUrl(url){
 }
 
 
+/**
+* Load the Gallery cards
+*/
+function loadGaleryDetails(childrenDetails, next){
+  console.log('Sheet: ' + SiteData.worksheets[0].title + ' [' + SiteData.worksheets[0].rowCount+' x '+SiteData.worksheets[0].colCount+']');
+
+  SiteData.worksheets[0].getRows({
+    offset: 1,
+    orderby: 'id',
+    'return-empty': false
+  }, function( err, rows ){
+
+    console.log('Read '+rows.length+' rows');
+    SiteData.cards = [];
+
+    rows.forEach(function(row){
+      var card = {
+          id: row.id,
+          title: row.title,
+          url: (!row.url ? defaultImgUrl : row.url),
+          smUrl: getThumbnailUrl((!row.url ? defaultImgUrl : row.url)),
+          desc: row.desc,
+          category: row.category,
+          imgs: ((childrenDetails[row.id] && row.category === "Architecture") ? childrenDetails[row.id] : [])
+      };
+
+      SiteData.cards.push(card);
+    });
+
+
+    // console.log("Data Initialized.");
+    if (next){
+      next();
+    }
+
+  });
+
+}
+
+/**
+* Load the Architecture Project images
+*/
+function loadGaleryItemDetails(next){
+  console.log('Sheet: ' + SiteData.worksheets[1].title+' ['+SiteData.worksheets[1].rowCount+' x '+SiteData.worksheets[1].colCount+']');
+
+  SiteData.worksheets[1].getRows({
+    offset: 1,
+    orderby: 'parentid',
+    'return-empty': false
+  }, function( err, rows ){
+
+    console.log('Read '+rows.length+' rows');
+    var imgs = [];
+
+    rows.forEach(function(row){
+      if (imgs[row.parentid]){
+        imgs[row.parentid].push(row.url);
+      } else {
+        imgs[row.parentid] = [row.url];
+      }
+    });
+
+
+    // console.log("Data Initialized.");
+    if (next){
+      next(imgs, SiteData.notifyEndLoad);
+    }
+
+  });
+}
+
 
 
 //-- Add functions
@@ -28,13 +99,18 @@ function getThumbnailUrl(url){
 * Function to load the details from the google spreadsheet into the SiteData object
 */
 SiteData.loadData = function( callback ) {
+
+  //-- Save the exit callback as method
+  SiteData.notifyEndLoad = callback;
+  SiteData.worksheets = [];
+
   /*
-  https://docs.google.com/spreadsheets/d/1aMYaGdm-3XVFPtHNPF49RQknq84ViiT_l1YCv226jck/edit?usp=sharing
   https://spreadsheets.google.com/feeds/list/1aMYaGdm-3XVFPtHNPF49RQknq84ViiT_l1YCv226jck/default/public/values?alt=json
   */
   var docId = "1aMYaGdm-3XVFPtHNPF49RQknq84ViiT_l1YCv226jck";
   var doc = new GoogleSpreadsheet(docId);
-  var sheet;
+  var sheetGalery;
+  var sheetChildren;
 
   // var defaultImgUrl = "https://dummyimage.com/283x211/f00/fff";
   var defaultImgUrl = "https://dummyimage.com/300x300/f00/fff";
@@ -44,37 +120,10 @@ SiteData.loadData = function( callback ) {
       console.log('Loaded doc: '+info.title+' by '+info.author.email);
 
       //-- Image galery details
-      sheet = info.worksheets[0];
-      console.log('Sheet 1: '+sheet.title+' ['+sheet.rowCount+' x '+sheet.colCount+']');
+      SiteData.worksheets = info.worksheets;
 
-      sheet.getRows({
-        offset: 1,
-        orderby: 'id',
-        'return-empty': false
-      }, function( err, rows ){
-
-        console.log('Read '+rows.length+' rows');
-        SiteData.imgs = [];
-
-        rows.forEach(function(row){
-          var card = {
-              id: row.id,
-              title: row.title,
-              url: (!row.url ? defaultImgUrl : row.url),
-              smUrl: getThumbnailUrl((!row.url ? defaultImgUrl : row.url)),
-              width: row.width,
-              height: row.height,
-              desc: row.desc,
-              category: row.category };
-          SiteData.imgs.push(card);
-        });
-
-        // console.log("Data Initialized.");
-        if (callback){
-          callback();
-        }
-
-      });
+      //-- Formar the nested data
+      loadGaleryItemDetails(loadGaleryDetails, callback);
 
     }else{
       console.error(err);
